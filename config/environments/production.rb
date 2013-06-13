bc07db3fec09dc7328988db7bc8529d3fc28fdff
duende84 <andreselduende@gmail.com>
2013-06-13 13:51:32 -0500

    configure production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-EvalComite::Application.configure do
+Cobit::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
